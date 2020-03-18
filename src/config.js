1805613fc23d72adc7e9113ff297c0816905b604
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-03-18 16:33:59 +0200

    Include more volto addons

--- src/config.js
+++ src/config.js
@@ -1,44 +1,24 @@
-/**
- * Add your config changes here.
- * @module config
- * @example
- * export const settings = {
- *   ...defaultSettings,
- *   port: 4300,
- *   listBlockTypes: {
- *     ...defaultSettings.listBlockTypes,
- *     'my-list-item',
- *   }
- * }
- */
 import * as voltoConfig from '@plone/volto/config';
 import {
   applyConfig as addonsConfig,
   // installImageSlides,
 } from 'volto-addons/config';
 
-import {
-  settings as defaultSettings,
-  views as defaultViews,
-  widgets as defaultWidgets,
-  blocks as defaultblocks,
-} from '@plone/volto/config';
-
 import HomepageView from '~/components/theme/HomepageView/HomepageView';
-// import { applyConfig as plotlyConfig } from 'volto-plotlycharts/config';
-// import { applyConfig as ckeditorConfig } from 'volto-ckeditor/config';
+import { applyConfig as plotlyConfig } from 'volto-plotlycharts/config';
+import { applyConfig as ckeditorConfig } from 'volto-ckeditor/config';
 // import { applyConfig as draftConfig } from 'volto-drafteditor/config';
-// import { applyConfig as mosaicConfig } from 'volto-mosaic/config';
-// import { applyConfig as dataBlocksConfig } from 'volto-datablocks/config';
+import { applyConfig as mosaicConfig } from 'volto-mosaic/config';
+import { applyConfig as dataBlocksConfig } from 'volto-datablocks/config';
 
 const config = [
   addonsConfig,
   // installImageSlides,
-  // plotlyConfig,
-  // ckeditorConfig,
+  plotlyConfig,
+  ckeditorConfig,
   // draftConfig,
-  // mosaicConfig,
-  // dataBlocksConfig,
+  mosaicConfig,
+  dataBlocksConfig,
 ].reduce((acc, apply) => apply(acc), voltoConfig);
 
 export const settings = {
