befc0a6e99186ce6833ef43a3f85020789a11d11
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-03-24 08:29:31 +0200

    Improve factsheetdatabaselisting

--- src/components/theme/FactsheetDatabaseListing/FactsheetDatabaseListing.jsx
+++ src/components/theme/FactsheetDatabaseListing/FactsheetDatabaseListing.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const FactsheetDatabaseListing = props => {
-  console.log('content', props.content);
-  return <div>Hello</div>;
+  const sections =
+    props.content['@components']?.['factsheet-database-listing'] || [];
+  return (
+    <div className="fdl">
+      {sections.map(section => (
+        <div className="fdl-listing-section" key={section['@id']}>
+          <h3>{section.title}</h3>
+          <table>
+            <thead>
+              {section.factsheet_group_title && (
+                <tr>
+                  <th>{''}</th>
+                  <th>{section.factsheet_group_title}</th>
+                </tr>
+              )}
+            </thead>
+            <tbody>
+              {section.items.map(item => (
+                <tr key={item['@id']}>
+                  <td>{item.title}</td>
+                  {section.factsheet_group_title && (
+                    <td>{item.factsheet_group}</td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
+    </div>
+  );
 };
+
 export default FactsheetDatabaseListing;
