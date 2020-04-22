f2a25bef4f6326fdbbeb1420932d2a77174858a4
David Silaghi <david.silaghi@eaudeweb.ro>
2020-04-22 16:08:16 +0300

    Refactored theme to simplify structure

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,1 @@
-import 'semantic-ui-less/semantic.less';
-// import '@plone/volto/../theme/theme.less';
-import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
+import '@plone/volto/theme';
