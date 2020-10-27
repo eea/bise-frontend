b64756b7e1d27ccf327de55094a4a7a82386476a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-27 20:09:46 +0200

    Use new eea/volto release

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 const baseCfg = require('@plone/volto/babel');
-// "react-loadable/babel"
 
-module.exports = function(api) {
+module.exports = function (api) {
   const voltoConfig = baseCfg(api);
   const presets = voltoConfig.presets;
   const plugins = [...voltoConfig.plugins];
